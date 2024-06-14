31985939ee9b1fb0545fd471aeaf7831a9e4d53f
JOhnSm1th900 <josephkias752@gmail.com>
2024-06-14 13:12:47 +0100

    registration & profile update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path,include
-
+from accounts.views import RegisterView, edit_profile
 from accounts.forms import UserLoginForm
 from accounts.views import LogoutViewWithGet
 
 urlpatterns = [
-    path('login/', LoginView.as_view(authentication_form=UserLoginForm),name='login'),
-    path('logout/', LogoutViewWithGet.as_view(), name='logout'),  # تمّ التعديل هنا
+    path('login/', LoginView.as_view(authentication_form=UserLoginForm), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', edit_profile, name='profile'),
+    path('logout/', LogoutViewWithGet.as_view(), name='logout'),
     path('', include('django.contrib.auth.urls'))
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import LogoutView
+from django.urls import reverse_lazy
+from accounts.forms import UserRegisterForm, ProfileForm
+from django.contrib.auth import login
+from django.views.generic import CreateView
+from django.contrib.auth.decorators import login_required
 
 class LogoutViewWithGet(LogoutView):
 
@@ -7,3 +12,26 @@ class LogoutViewWithGet(LogoutView):
 
     def get(self, request, *args, **kwargs):
         return self.post(request, *args, **kwargs)
+
+class RegisterView(CreateView):
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
+
+    def get_success_url(self):
+        login(self.request, self.object)
+        return reverse_lazy('Project_list')
+
+
+@login_required()
+def edit_profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=request.user)
+        return render(request, 'profile.html', {
+            'form': form
+        })
