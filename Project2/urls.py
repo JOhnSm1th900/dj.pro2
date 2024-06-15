882462f18aa50d6fd76d5af5d9e999e8efcbc82c
JOhnSm1th900 <josephkias752@gmail.com>
2024-06-15 08:38:57 +0100

    Admin Panel

--- Project2/urls.py
+++ Project2/urls.py
@@ -2,6 +2,11 @@ import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
+
+
+admin.site.site_header = ('Projects Management')
+admin.site.site_title = ('Projects Management')
+
 urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
     path('', include('Project.urls')),
